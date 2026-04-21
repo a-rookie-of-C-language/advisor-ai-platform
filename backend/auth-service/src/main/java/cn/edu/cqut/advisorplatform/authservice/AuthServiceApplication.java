5d4fdd765e68065d234657344d10497f6725ee13
arookieofc <2128194521@qq.com>
2026-04-21 09:08:04 +0800

    fix: 修复微服务e2e启动链路中的gateway MVC冲突与JPA/Feign依赖问题

--- backend/auth-service/src/main/java/cn/edu/cqut/advisorplatform/authservice/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/cn/edu/cqut/advisorplatform/authservice/AuthServiceApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "cn.edu.cqut.advisorplatform")
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableJpaRepositories(basePackages = "cn.edu.cqut.advisorplatform.dao")
+@EntityScan(basePackages = "cn.edu.cqut.advisorplatform.entity")
 public class AuthServiceApplication {
 
   public static void main(String[] args) {
