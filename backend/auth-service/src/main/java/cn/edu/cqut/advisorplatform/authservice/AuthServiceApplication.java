fe486961c010195f001014466192cd318496d212
arookieofc <2128194521@qq.com>
2026-04-21 09:11:01 +0800

    style: 执行spotless自动格式化修复CI格式校验失败

--- backend/auth-service/src/main/java/cn/edu/cqut/advisorplatform/authservice/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/cn/edu/cqut/advisorplatform/authservice/AuthServiceApplication.java
@@ -2,9 +2,9 @@ package cn.edu.cqut.advisorplatform.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "cn.edu.cqut.advisorplatform")
