1f9393f3a68e41e71092976a7d28fbfb9f761a6a
arookieofc <2128194521@qq.com>
2026-04-27 19:48:10 +0800

    fix: 修复memory-service测试类包路径不一致导致的编译失败

--- backend/memory-service/src/main/java/cn/edu/cqut/advisorplatform/service/impl/MemoryServiceImpl.java
+++ backend/memory-service/src/main/java/cn/edu/cqut/advisorplatform/service/impl/MemoryServiceImpl.java
@@ -1,9 +1,5 @@
 package cn.edu.cqut.advisorplatform.service.impl;
 
-import cn.edu.cqut.advisorplatform.memoryservice.dao.ChatSessionDao;
-import cn.edu.cqut.advisorplatform.memoryservice.dao.MemoryTaskDao;
-import cn.edu.cqut.advisorplatform.memoryservice.dao.SessionSummaryDao;
-import cn.edu.cqut.advisorplatform.memoryservice.dao.UserMemoryDao;
 import cn.edu.cqut.advisorplatform.dto.request.MemoryCandidateItemDTO;
 import cn.edu.cqut.advisorplatform.dto.request.MemoryCandidateUpsertRequestDTO;
 import cn.edu.cqut.advisorplatform.dto.request.MemorySearchRequestDTO;
@@ -13,12 +9,16 @@ import cn.edu.cqut.advisorplatform.dto.response.MemoryCandidateUpsertResponseDTO
 import cn.edu.cqut.advisorplatform.dto.response.MemoryItemResponseDTO;
 import cn.edu.cqut.advisorplatform.dto.response.MemoryTaskResponseDTO;
 import cn.edu.cqut.advisorplatform.dto.response.SessionSummaryResponseDTO;
+import cn.edu.cqut.advisorplatform.exception.BadRequestException;
+import cn.edu.cqut.advisorplatform.exception.NotFoundException;
+import cn.edu.cqut.advisorplatform.memoryservice.dao.ChatSessionDao;
+import cn.edu.cqut.advisorplatform.memoryservice.dao.MemoryTaskDao;
+import cn.edu.cqut.advisorplatform.memoryservice.dao.SessionSummaryDao;
+import cn.edu.cqut.advisorplatform.memoryservice.dao.UserMemoryDao;
 import cn.edu.cqut.advisorplatform.memoryservice.entity.ChatSessionDO;
 import cn.edu.cqut.advisorplatform.memoryservice.entity.MemoryTaskDO;
 import cn.edu.cqut.advisorplatform.memoryservice.entity.SessionSummaryDO;
 import cn.edu.cqut.advisorplatform.memoryservice.entity.UserMemoryDO;
-import cn.edu.cqut.advisorplatform.exception.BadRequestException;
-import cn.edu.cqut.advisorplatform.exception.NotFoundException;
 import cn.edu.cqut.advisorplatform.service.MemoryService;
 import cn.edu.cqut.advisorplatform.service.vector.EmbeddingService;
 import cn.edu.cqut.advisorplatform.service.vector.MemoryServiceFactory;
