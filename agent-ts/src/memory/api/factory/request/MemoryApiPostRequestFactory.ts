d76a7ef35e2be1abba15811635dcd75a8607b7ee
arookieofc <2128194521@qq.com>
2026-08-08 12:58:54 +0800

    refactor: 归类记忆 API 工厂模块

--- agent-ts/src/memory/api/factory/request/MemoryApiPostRequestFactory.ts
+++ agent-ts/src/memory/api/factory/request/MemoryApiPostRequestFactory.ts
@@ -1,6 +1,6 @@
-import type { JsonObject } from "../../../common/json/JsonTypes.js";
-import type { MemoryCandidateUpsertRequest } from "../../model/request/api/MemoryCandidateUpsertRequest.js";
-import type { MemoryTaskSubmitRequest } from "../../model/task/MemoryTaskSubmitRequest.js";
+import type { JsonObject } from "../../../../common/json/JsonTypes.js";
+import type { MemoryCandidateUpsertRequest } from "../../../model/request/api/MemoryCandidateUpsertRequest.js";
+import type { MemoryTaskSubmitRequest } from "../../../model/task/MemoryTaskSubmitRequest.js";
 
 export class MemoryApiPostRequestFactory {
   createLongTermSearch(userId: number, kbId: number, query: string, topK: number): RequestInit {
