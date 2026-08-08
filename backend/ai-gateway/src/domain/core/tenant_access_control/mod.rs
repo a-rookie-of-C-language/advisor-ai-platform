34ed80ebb197d59efada5dcba80f737d3018b32f
arookieofc <2128194521@qq.com>
2026-08-08 16:21:23 +0800

    refactor: 归类租户访问控制模型模块

--- backend/ai-gateway/src/domain/core/tenant_access_control/mod.rs
+++ backend/ai-gateway/src/domain/core/tenant_access_control/mod.rs
@@ -1,9 +1,9 @@
 #[allow(non_snake_case)]
-#[path = "model/Tenant.rs"]
+#[path = "model/entity/Tenant.rs"]
 pub mod Tenant;
 #[allow(non_snake_case)]
 #[path = "dao/TenantDao.rs"]
 pub mod TenantDao;
 #[allow(non_snake_case)]
-#[path = "model/TenantIdentity.rs"]
+#[path = "model/identity/TenantIdentity.rs"]
 pub mod TenantIdentity;
